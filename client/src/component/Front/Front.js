43504042297506f171bf1102ecc4d88643a7bc04
shivang <shivangnov@gmail.com>
2023-06-22 23:43:08 +0530

    added loading filler

--- client/src/component/Front/Front.js
+++ client/src/component/Front/Front.js
@@ -1,41 +1,42 @@
-import React, { Fragment, useEffect } from 'react'
-import {CgMouse} from "react-icons/cg"
-import "./Front.css"
-import Product from "./Product.js"
-import MetaData from '../layout/MetaData'
-import {getProduct} from "../../actions/productAction"
-import {useSelector,useDispatch} from "react-redux"
-
-
-
+import React, { Fragment, useEffect } from "react";
+import { CgMouse } from "react-icons/cg";
+import "./Front.css";
+import Product from "./Product.js";
+import MetaData from "../layout/MetaData";
+import Loader from '../layout/Loader/Loader';
+import { getProduct } from "../../actions/productAction";
+import { useSelector, useDispatch } from "react-redux";
 
 const Front = () => {
-  const dispatch = useDispatch()
-  const {loading,error,products,productsCount} = useSelector(state=>state.products)
-  useEffect(()=>{
-    dispatch(getProduct())
-  },[dispatch])
+  const dispatch = useDispatch();
+  const { loading, error, products, productsCount } = useSelector(
+    (state) => state.products
+  );
+  useEffect(() => {
+    dispatch(getProduct());
+  }, [dispatch]);
   return (
     <Fragment>
-      <MetaData title="UN-STOP"/>
-        <div className='banner'>
+      {loading ?  <Loader /> :(
+      <Fragment>
+        <MetaData title="UN-STOP" />
+        <div className="banner">
           <p>Welcome to UN-STOP</p>
           <h1>FIND AMAZING PRODUCTS BELOW</h1>
-            <a href="#container">
-                <button>
-                    Scroll <CgMouse />
-                </button> 
-            </a>
+          <a href="#container">
+            <button>
+              Scroll <CgMouse />
+            </button>
+          </a>
         </div>
-        <h2 className='homeHeading'>Featured Products</h2>
-        <div className='container' id="container">
-          {products && products.map(product=>(
-            <Product product={product} />
-          ))}
-
+        <h2 className="homeHeading">Featured Products</h2>
+        <div className="container" id="container">
+          {products && products.map((product) => <Product product={product} />)}
         </div>
+      </Fragment>
+      )}
     </Fragment>
   )
-}
+};
 
-export default Front
+export default Front;
